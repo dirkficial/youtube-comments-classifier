f1ade64296f54224f80f2c970be6eb6cc5bb92f9
Derek Kim <derek.kim175@gmail.com>
2026-04-01 16:42:18 +0900

    fix: wrong imports, username field, typo

--- app/routes/users.py
+++ app/routes/users.py
@@ -1,36 +1,36 @@
 from typing import Annotated
-from fastapi import APIRouter, Depends, HTTPException
-from requests import Session
-from sqlalchemy import select, models
-from schemas import (UserResponse, UserCreate)
-from db import get_db
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+import app.models as models
+from app.schemas import (UserResponse, UserCreate)
+from app.db import get_db
 
 router = APIRouter(prefix="/api/users", tags=["users"])
 
 @router.post("/",
              response_model=UserResponse,
-             status_code=201
+             status_code=status.HTTP_201_CREATED
              )
 def create_user(user: UserCreate, db: Annotated[Session, Depends(get_db)]):
     result = db.execute(
-        select(models.User).where(models.User.username == user.username)
-        )
-    existing_user = result.scalars().first()
-    
-    if existing_user:
-        raise HTTPException(
-            status_code=400,
-            detail="Username already exists"
-        )
-    
-    result = db.execute(
         select(models.User).where(models.User.email == user.email)
         )
-    existing_email = result.scalars().first()
-    
-    if existing_email:
+    existing_user = result.scalars().first()
+
+    if existing_user:
         raise HTTPException(
-            status_code=400,
+            status_code=status.HTTP_400_BAD_REQUEST,
             detail="Email already exists"
         )
-    
+
+    new_user = models.User(
+        email=user.email,
+        hashed_password=user.password
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
