7990db894191931494d979377cbebe4d06044283
Derek Kim <derek.kim175@gmail.com>
2026-04-01 14:48:05 +0900

    renamed to db

---
+++ db.py
@@ -0,0 +1,22 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+
+load_dotenv()
+
+SQL_ALCHEMY_DATABASE_URL = os.get("DATABASE_URL", "sqlite:///./blog.db")
+
+engine = create_engine(
+    SQL_ALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False},
+)
+
+SessionLocal = sessionmaker(autocommit = False, autoflush = False, bind=engine)
+
+class Base(DeclarativeBase):
+    pass
+
+def get_db():
+    with SessionLocal() as db:
+        yield db
